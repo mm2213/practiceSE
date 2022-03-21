95dbd3b0a9eaf01562d36608399a0012e5255087
Mihir Mehta <contact.mihir2213@gmail.com>
2022-03-21 21:53:19 +0530

    Create CommandExecutor.java

---
+++ src/main/java/com/commands/CommandExecutor.java
@@ -0,0 +1,17 @@
+package com.mihir.commands;
+
+import com.mihir.model.Command;
+
+public class CommandExecutor {
+
+	public boolean validate(Command command) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void execute(Command command) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
