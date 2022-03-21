80d2fb1428830cb492cab87cc5dec3ed2f5b72f0
Mihir Mehta <contact.mihir2213@gmail.com>
2022-03-21 22:03:50 +0530

    Add files via upload

---
+++ src/main/java/com/mihir/commands/CommandExecutorFactory.java
@@ -0,0 +1,17 @@
+package com.mihir.commands;
+
+import com.mihir.model.Command;
+import com.mihir.service.ParkingLotService;
+
+public class CommandExecutorFactory {
+
+	public CommandExecutorFactory(ParkingLotService parkingLotService) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public CommandExecutor getCommandExecutor(Command command) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
