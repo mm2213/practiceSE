2e06f502c64f0c23daee976f26bcef7bba938f7c
Mihir Mehta <contact.mihir2213@gmail.com>
2022-04-12 13:03:50 +0530

    Add files via upload

--- src/main/java/com/mihir/mode/FileMode.java
+++ src/main/java/com/mihir/mode/FileMode.java
@@ -1,16 +1,41 @@
 package com.mihir.mode;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 import com.mihir.OutputPrinter;
 import com.mihir.commands.CommandExecutorFactory;
+import com.mihir.model.Command;
 
-public class FileMode {
-
-	public FileMode(CommandExecutorFactory commandExecutorFactory, OutputPrinter outputPrinter, String string) {
+// Mode running in which commands are given from a file
+public class FileMode extends Mode {
+	private String fileName;
+	
+	public FileMode(final CommandExecutorFactory commandExecutorFactory, final OutputPrinter outputPrinter, final String fileName) {
 		// TODO Auto-generated constructor stub
+		super(commandExecutorFactory,outputPrinter);
+		this.fileName=fileName;
 	}
 
-	public void process() {
+	@Override
+	public void process() throws IOException {
 		// TODO Auto-generated method stub
+		final File file= new File(fileName);
+		final BufferedReader reader;
+		
+		reader= new BufferedReader(new FileReader(file));
+		
+		String input = reader.readLine();
+		while(input != null) {
+			final Command command= new Command(input);
+			processCommand(command);
+			input = reader.readLine();
+		}
+		
+		
 		
 	}
 
--- src/main/java/com/mihir/mode/InteractiveMode.java
+++ src/main/java/com/mihir/mode/InteractiveMode.java
@@ -1,9 +1,13 @@
 package com.mihir.mode;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import com.mihir.OutputPrinter;
 import com.mihir.commands.CommandExecutorFactory;
+import com.mihir.commands.ExitCommandExecutor;
+import com.mihir.model.Command;
 
 public class InteractiveMode extends Mode {
 
@@ -13,7 +17,16 @@ public class InteractiveMode extends Mode {
 
 	public void process() throws IOException{
 		// TODO Auto-generated method stub
-		
+		outputPrinter.welcome();
+		final BufferedReader reader= new BufferedReader(new InputStreamReader(System.in));
+		while(true) {
+			final String input =reader.readLine();
+			final Command command= new Command(input);
+			processCommand(command);
+			if(command.getCommandName().equals(ExitCommandExecutor.COMMAND_NAME));{
+				break;
+			}
+		}
 	}
 
 }
